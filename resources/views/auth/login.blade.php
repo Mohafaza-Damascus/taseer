958359698ba058df87e81b4af0d3652cc538f849
kinannotfound2021 <kinannotfound2021@gmail.com>
2026-09-17 12:26:06 +0300

    railway

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,9 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>تسجيل الدخول</title>
-    @vite('resources/css/variables.css')
-    @vite('resources/css/auth/login.css')
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
