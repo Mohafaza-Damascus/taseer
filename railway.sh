8fe281d3109542e1710383be8f3a3a36ac58fd10
kinannotfound2021 <kinannotfound2021@gmail.com>
2026-09-20 14:06:36 +0300

    railway

--- railway.sh
+++ railway.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
-set -e
-
-composer install --no-dev --optimize-autoloader
-
-npm install
-npm run build
-
+php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-php artisan migrate --force
-
-php artisan db:seed --force
-
-php artisan storage:link
+php artisan migrate:fresh --seed
+npm install
+npm run build
+npm run dev
+php artisan serve --host=0.0.0.0 --port=$PORT
