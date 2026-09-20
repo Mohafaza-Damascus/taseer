26f87a2af4ffd1b5efb8d828b057d7e60b3133d7
kinannotfound2021 <kinannotfound2021@gmail.com>
2026-09-20 13:39:27 +0300

    railway

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-import './echo';
-import.meta.glob([
-  '../images/**',
-]);
+// resources/js/app.js
+import '../css/app.css';
 import.meta.glob('../css/**/*.css', { eager: true });
-import.meta.glob('../css/*.css', { eager: true });
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>تسجيل الدخول</title>
-    <link rel="stylesheet" href="resources/css/variable.css">
-    <link rel="stylesheet" href="resources/css/auth/login.css">
-    {{-- @vite('resources/css/variables.css') --}}
-    {{-- @vite('resources/css/auth/login.css') --}}
+    @vite('resources/css/variables.css')
+    @vite('resources/css/auth/login.css')
 </head>
 
 <body>
